25e5677450fd8170dc27034fd1713249ba0a9ab5
Cotito27 <65867363+Cotito27@users.noreply.github.com>
2021-02-21 03:55:55 -0500

    test 5

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ app.set('view engine', 'ejs');
 
 app.use(morgan('dev'));
 let corsOptions = {
-  origin: 'https://cotipelistv.herokuapp.com/',
+  origin: 'https://localhost:3000/',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 app.use(cors(corsOptions));
