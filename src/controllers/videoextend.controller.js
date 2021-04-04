5ec4bc56102a21622b3ee0f9e9c093dbb9fc6b4c
Cotito27 <65867363+Cotito27@users.noreply.github.com>
2021-04-04 16:38:19 -0500

    version 12

--- src/controllers/videoextend.controller.js
+++ src/controllers/videoextend.controller.js
@@ -41,7 +41,7 @@ async function getVideoUrl(url) {
 }
 
 async function downloadVideo(url) {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: true,args: ['--no-sandbox','--disable-setuid-sandbox'] });
   const page = await browser.newPage();
 
   let urlDownload = url;
